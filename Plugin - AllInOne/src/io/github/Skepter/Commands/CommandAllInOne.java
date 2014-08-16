40d768ea1d191a70f6b2f08ff3206a8a9739e9eb
Skepter <skepter.misc@gmail.com>
2014-08-16 19:23:25 +0100

    Adjust aio cmd

--- Plugin - AllInOne/src/io/github/Skepter/Commands/CommandAllInOne.java
+++ Plugin - AllInOne/src/io/github/Skepter/Commands/CommandAllInOne.java
@@ -40,9 +40,8 @@ public class CommandAllInOne {
 	@CommandHandler(name = "allinone.commands", aliases = { "aio.cmds", "allinone.cmds", "aio.commands" }, permission = "AllInOne.allinone", description = "Views plugin commands", usage = "Use <command>")
 	public void commands(final CommandArgs args) {
 		int arg = 1;
-		if (!(args.getArgs().length == 0)) {
+		if (args.getArgs().length == 1)
 			arg = Integer.parseInt(args.getArgs()[0]);
-		}
 
 		TextUtils.paginate(args.getSender(), CommandFramework.pluginCommands, 10, arg);
 		return;
@@ -54,7 +53,7 @@ public class CommandAllInOne {
 		final Timer timer = new Timer();
 		/* We're currently in dev and dev file name isn't the same as the released name */
 		final File devPluginFile = new File(AllInOne.instance().getDataFolder().getParent() + File.separator + "AllInOne.jar");
-//		final File pluginFile = new File(AllInOne.instance().getDataFolder().getParent() + File.separator + "AllInOne-" + AllInOne.instance().getDescription().getVersion() + ".jar");
+		//		final File pluginFile = new File(AllInOne.instance().getDataFolder().getParent() + File.separator + "AllInOne-" + AllInOne.instance().getDescription().getVersion() + ".jar");
 		final String cachedTitle = AllInOne.instance().title;
 		timer.schedule(new TimerTask() {
 
@@ -64,7 +63,8 @@ public class CommandAllInOne {
 					args.getSender().sendMessage(cachedTitle + "AllInOne successfully reloaded");
 					Bukkit.getPluginManager().loadPlugin(devPluginFile);
 					//Bukkit.getPluginManager().enablePlugin(AllInOne.instance());
-				} catch (final UnknownDependencyException | InvalidPluginException | InvalidDescriptionException e) {
+				} catch (final UnknownDependencyException
+						| InvalidPluginException | InvalidDescriptionException e) {
 					e.printStackTrace();
 				}
 			}
