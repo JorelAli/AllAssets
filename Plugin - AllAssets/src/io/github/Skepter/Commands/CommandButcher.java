b4b61622a1025ed8ad12ce9ec9d3fce3e475a809
Skepter <skepter.misc@gmail.com>
2014-08-30 10:04:01 +0100

    Added a few flags to CommandButcher

--- Plugin - AllAssets/src/io/github/Skepter/Commands/CommandButcher.java
+++ Plugin - AllAssets/src/io/github/Skepter/Commands/CommandButcher.java
@@ -6,8 +6,10 @@ import io.github.Skepter.Commands.CommandFramework.CommandHandler;
 
 import java.util.Iterator;
 
+import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 
 public class CommandButcher {
@@ -19,11 +21,31 @@ public class CommandButcher {
 	@CommandHandler(name = "butcher", aliases = { "killall" }, permission = "butcher", description = "Kills mobs", usage = "Use <command>")
 	public void onCommand(final CommandArgs args) {
 		final Player player = args.getPlayer();
+		boolean lightning, passive, hostile;
+		lightning = passive = hostile = false;
+		if (args.getArgs().length > 0) {
+			for (String s : args.getArgs()) {
+				if (s.contains("-l"))
+					lightning = true;
+				if (s.contains("-p"))
+					passive = true;
+				if (s.contains("-h"))
+					hostile = true;
+			}
+		}
 		int count = 0;
 		Iterator<Entity> iterator = player.getWorld().getEntities().iterator();
-		while (iterator.hasNext()){
-			Entity entity = iterator.next();
-			if (entity instanceof LivingEntity) {
+		while (iterator.hasNext()) {
+			Entity entity = (Entity) iterator.next();
+			if (entity instanceof LivingEntity && !(entity instanceof Player)) {
+				if (passive)
+					if (entity instanceof Monster)
+						continue;
+				if (hostile)
+					if (entity instanceof Animals)
+						continue;
+				if (lightning)
+					entity.getWorld().strikeLightningEffect(entity.getLocation());
 				entity.remove();
 				count++;
 			}
