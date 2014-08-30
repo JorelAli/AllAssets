46c277e0970a2bc6e6014a3504821d1ab989c358
Skepter <skepter.misc@gmail.com>
2014-08-30 22:26:51 +0100

    Created LocationUtils

---
+++ Plugin - AllAssets/src/io/github/Skepter/Utils/LocationUtils.java
@@ -0,0 +1,9 @@
+package io.github.Skepter.Utils;
+
+public class LocationUtils {
+
+	/* Used to calculate locations
+	 * E.g. exact spawn location for a mob
+	 * Location to teleport a player using asc/desc
+	 * Checking if the target location is safe or not */
+}
