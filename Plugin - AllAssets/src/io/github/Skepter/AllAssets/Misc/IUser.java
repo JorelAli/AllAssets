cd64b1a996b28ec2604e701903b3eb40a1686061
Skepter <skepter.misc@gmail.com>
2014-11-05 17:48:14 +0000

    After researching, abstract isn't needed

--- Plugin - AllAssets/src/io/github/Skepter/AllAssets/Misc/IUser.java
+++ Plugin - AllAssets/src/io/github/Skepter/AllAssets/Misc/IUser.java
@@ -38,78 +38,78 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
-public abstract interface IUser {
+public interface IUser {
 
 	/** Gets the last location of a player */
-	public abstract Location getLastLoc();
+	public Location getLastLoc();
 
 	/** Sets the last location for a player at their current location */
-	public abstract void setLastLoc();
+	public void setLastLoc();
 
 	/** Sets the last location for a player at a specific location */
-	public abstract void setLastLoc(Location loc);
+	public void setLastLoc(Location loc);
 
 	/** Gets a home location from the home name */
-	public abstract Location getHome(String name);
+	public Location getHome(String name);
 
 	/** Sets a home as the specified location and name */
-	public abstract void setHome(Location loc, String name);
+	public void setHome(Location loc, String name);
 
 	/** Gets a waypoint from a specific location */
-	public abstract Location getWaypoint(Location loc);
+	public Location getWaypoint(Location loc);
 
-	public abstract void setWaypoint(Location loc);
+	public void setWaypoint(Location loc);
 
-	public abstract Player getPlayer();
+	public Player getPlayer();
 
-	public abstract Long getTimeSinceLastPlay();
+	public Long getTimeSinceLastPlay();
 
-	public abstract Long getTotalTimePlayed();
+	public Long getTotalTimePlayed();
 
-	public abstract boolean canTp();
+	public boolean canTp();
 
-	public abstract void setTotalTimePlayed(long time);
+	public void setTotalTimePlayed(long time);
 
-	public abstract void setTimeSinceLastPlay(long time);
+	public void setTimeSinceLastPlay(long time);
 
-	public abstract int getAttackStrength();
+	public int getAttackStrength();
 
-	public abstract void setAttackStrength(int strength);
+	public void setAttackStrength(int strength);
 
-	public abstract int getJumpPower();
+	public int getJumpPower();
 
-	public abstract void setJumpPower(int power);
+	public void setJumpPower(int power);
 
-	public abstract Inventory getLastInventory();
+	public Inventory getLastInventory();
 
-	public abstract void setLastInventory(String s);
+	public void setLastInventory(String s);
 
-	public abstract int getDeathCount();
+	public int getDeathCount();
 
-	public abstract void setDeathCount(int i);
+	public void setDeathCount(int i);
 
 	/** Gets the amount of times a player has joined */
-	public abstract int getJoinCount();
+	public int getJoinCount();
 
 	/** Sets the amount of times a player has joined */
-	public abstract void setJoinCount(int joinCount);
+	public void setJoinCount(int joinCount);
 
-	public abstract List<String> IPs();
+	public List<String> IPs();
 
-	public abstract void setIPs(List<String> arr);
+	public void setIPs(List<String> arr);
 
-	public abstract void setAFK(boolean afk);
+	public void setAFK(boolean afk);
 
-	public abstract boolean isAFK();
+	public boolean isAFK();
 	
-	public abstract List<UUID> getFriendList();
+	public List<UUID> getFriendList();
 	
-	public abstract void setFriendList();
+	public void setFriendList();
 	
-	public abstract String getLanguage(Player p);
+	public String getLanguage(Player p);
 
-	public abstract boolean canTP();
+	public boolean canTP();
 
-	public abstract void setCanTP(boolean tp);
+	public void setCanTP(boolean tp);
 
 }
