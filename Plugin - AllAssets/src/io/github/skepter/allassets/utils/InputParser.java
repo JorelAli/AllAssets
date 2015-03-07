4c1d5e1bcce01968e5a425410a7d777b5347c5ab
Skepter <skepter.misc@gmail.com>
2015-03-07 23:01:06 +0000

    fixed spawnmob bugs

--- Plugin - AllAssets/src/io/github/skepter/allassets/utils/InputParser.java
+++ Plugin - AllAssets/src/io/github/skepter/allassets/utils/InputParser.java
@@ -5,15 +5,20 @@ import org.bukkit.entity.EntityType;
 public class InputParser {
 
 	private String inputString;
-	
+
 	public InputParser(String inputString) {
 		this.inputString = inputString;
 	}
-	
+
 	public EntityType parseMob() {
-		if(EntityType.valueOf(inputString) != null)
-			return EntityType.valueOf(inputString);
-		switch(inputString.toLowerCase()) {
+		if (inputString == null)
+			return EntityType.UNKNOWN;
+		try {
+			if (EntityType.valueOf(inputString.toUpperCase()) != null)
+				return EntityType.valueOf(inputString.toUpperCase());
+		} catch (Exception e) {
+		}
+		switch (inputString.toLowerCase()) {
 		case "zombiepigman":
 		case "pigzombie":
 			return EntityType.PIG_ZOMBIE;
