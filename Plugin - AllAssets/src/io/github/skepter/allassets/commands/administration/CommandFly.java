c2d26cea770db63538e8654321f3f28ec9833e45
Skepter <skepter.misc@gmail.com>
2015-03-08 00:41:51 -0800

    Updated all licences. (Phew 381 files?!)

--- Plugin - AllAssets/src/io/github/skepter/allassets/commands/administration/CommandFly.java
+++ Plugin - AllAssets/src/io/github/skepter/allassets/commands/administration/CommandFly.java
@@ -1,32 +1,24 @@
 /*******************************************************************************
- * Skepter's licence
- * Copyright 2015 
- *
- * AllAssets was created by Skepter and Tundra (http://skepter.github.io/).
- *
+ * Skepter's Licence
+ * Copyright © 2015
+ * 
+ * AllAssets, created by Skepter and Tundra
+ * 
  * You are able to:
- * * View AllAsset's source code on github
- * * Experiment with the code to your wish
- * * Download it to use on your server
- *
- * You are NOT able to:
- * * Sell AllAssets - it is COMPLETELY free for ALL users
- * * Claim it as your own. AllAssets is created by Skepter
- * * Distribute it on any other website. The ONLY places where it can be downloaded from is github and the Bukkit repository
- * * Decompile the code - it's COMPLETELY unnecessary since the entire code is on Github. The only exception is the BukkitDev staff who I have allowed permission to decompile (to search for malicious code)
- *
- * You can not:
- * * Hold me liable for your actions:
- *     - If there is a bug in the program, you are allowed to notify me.
- *     - If other players abuse AllAssets, you are NOT allowed to hold me liable for that
- *     - If it was setup incorrectly or you used a non-official build, you can NOT hold me liable for that.
- * * Yell at me:
- *     - If something is wrong, please approach the situation calmly.
- * * Modify the code:
- *     - You are NOT allowed to 'steal' the whole code from Github, modify it, compile it yourself and distribute that.
- *
- * If you are to break from these implications, future use of this plugin will be forbidden.
- *******************************************************************************/
+ * * View AllAssets' source code on GitHub
+ * * Experiment with the code as you wish
+ * * Download the .jar files supplied on GitHub for your server
+ * 
+ * You are NOT allowed to:
+ * * Sell AllAssets - It is COMPLETELY free for ALL users
+ * * Claim it as your own. AllAssets is created by Skepter and Tundra
+ * * Distribute it on any other website
+ * * Decompile the code - It's pointless, time consuming and the source code is already on GitHub
+ * * Steal the code from GitHub. Just ask and we're more than likely to let you copy some of it
+ * 
+ * You cannot:
+ * * Hold us liable for your actions
+ ******************************************************************************/
 /*******************************************************************************
  *******************************************************************************/
 /*******************************************************************************
@@ -34,9 +26,9 @@
 package io.github.skepter.allassets.commands.administration;
 
 import io.github.skepter.allassets.CommandFramework;
-import io.github.skepter.allassets.PlayerGetter;
 import io.github.skepter.allassets.CommandFramework.CommandArgs;
 import io.github.skepter.allassets.CommandFramework.CommandHandler;
+import io.github.skepter.allassets.PlayerGetter;
 import io.github.skepter.allassets.utils.Strings;
 import io.github.skepter.allassets.utils.utilclasses.ErrorUtils;
 
