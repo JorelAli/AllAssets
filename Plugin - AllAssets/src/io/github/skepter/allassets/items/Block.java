7b7098e8a7e7d6218f2f2ef7d903cf51f914f6f7
Skepter <skepter.misc@gmail.com>
2015-04-12 20:46:16 +0100

    Cured by OCD of having under 3 classes in a package

---
+++ Plugin - AllAssets/src/io/github/skepter/allassets/items/Block.java
@@ -0,0 +1,11 @@
+package io.github.skepter.allassets.items;
+
+public class Block {
+
+	/*
+	 * I just wanted a third class in this package.
+	 * I think I have OCD.
+	 * It just felt weird having 2 classes and it has to be changed.
+	 */
+	
+}
