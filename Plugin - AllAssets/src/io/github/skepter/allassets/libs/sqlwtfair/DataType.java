585867c3ef2c4b076db231adbfc09728c9463f87
Skepter <skepter.misc@gmail.com>
2015-04-26 11:18:39 +0100

    Applied copyright to new classes

--- Plugin - AllAssets/src/io/github/skepter/allassets/libs/sqlwtfair/DataType.java
+++ Plugin - AllAssets/src/io/github/skepter/allassets/libs/sqlwtfair/DataType.java
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * Skepter's Licence
+ * Copyright © 2015
+ * 
+ * AllAssets, created by Skepter and Tundra
+ * 
+ * You are able to:
+ * * View AllAssets' source code on GitHub
+ * * Experiment with the code as you wish
+ * * Download the .jar files supplied on GitHub for your server
+ * 
+ * You are NOT allowed to:
+ * * Sell AllAssets - It is COMPLETELY free for ALL users
+ * * Claim it as your own. AllAssets is created by Skepter and Tundra
+ * * Distribute it on any other website
+ * * Decompile the code - It's pointless, time consuming and the source code is already on GitHub
+ * * Steal the code from GitHub. Just ask and we're more than likely to let you copy some of it
+ * 
+ * You cannot:
+ * * Hold us liable for your actions
+ ******************************************************************************/
 package io.github.skepter.allassets.libs.sqlwtfair;
 
 /** List of all MySQL datatypes. Descriptions taken from <a
