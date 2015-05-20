a6aa031eab0ef4ae2c5a7265b04f5ca652a11a39
Skepter <skepter.misc@gmail.com>
2015-05-20 08:51:01 +0100

    Makes preparations for NMS implementation system

---
+++ Plugin - AllAssets/src/io/github/skepter/allassets/version/NMS.java
@@ -0,0 +1,10 @@
+package io.github.skepter.allassets.version;
+
+/*
+ * See
+ * https://github.com/deathmarine/RefactorInterfaceExample/tree/master/src/com/modcrafting/example
+ */
+
+public interface NMS {
+
+}
