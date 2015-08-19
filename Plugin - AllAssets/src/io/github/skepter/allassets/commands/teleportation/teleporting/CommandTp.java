09cd2ef6358194627ed662f974a2ee72d0f90cdf
Skepter <skepter.misc@gmail.com>
2015-08-19 08:31:30 +0100

    Temporarily disabled offline teleporting

--- Plugin - AllAssets/src/io/github/skepter/allassets/commands/teleportation/teleporting/CommandTp.java
+++ Plugin - AllAssets/src/io/github/skepter/allassets/commands/teleportation/teleporting/CommandTp.java
@@ -34,7 +34,6 @@ import io.github.skepter.allassets.utils.Strings;
 import io.github.skepter.allassets.utils.utilclasses.ErrorUtils;
 import io.github.skepter.allassets.utils.utilclasses.PlayerUtils;
 
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 public class CommandTp {
@@ -52,22 +51,22 @@ public class CommandTp {
 				return;
 			}
 			final Player onlineTarget = PlayerUtils.getOnlinePlayerFromString(args.getArgs()[0]);
-			final OfflinePlayer offlineTarget = PlayerUtils.getOfflinePlayerFromStringExact(args.getArgs()[0]);
-			if (offlineTarget == null && onlineTarget == null) {
+//			final OfflinePlayer offlineTarget = PlayerUtils.getOfflinePlayerFromStringExact(args.getArgs()[0]);
+			if (/*offlineTarget == null && */onlineTarget == null) {
 				ErrorUtils.playerNotFound(args.getSender(), args.getArgs()[0]);
 				return;
 			}
 			final User user = new User(player);
-			final User target = new User(offlineTarget);
+			//final User target = new User(offlineTarget);
 			if (user.canTp()) {//TODO sort this out - user can tp? target can tp? which one?!
 				user.setLastLoc();
-				if (onlineTarget == null) {
-					player.teleport(target.getLastLoc());
-					player.sendMessage(Strings.TITLE + "Successfully teleported to " + offlineTarget.getName());
-				} else {
+//				if (onlineTarget == null) {
+//					player.teleport(target.getLastLoc());
+//					player.sendMessage(Strings.TITLE + "Successfully teleported to " + offlineTarget.getName());
+//				} else {
 					player.teleport(onlineTarget);
 					player.sendMessage(Strings.TITLE + "Successfully teleported to " + onlineTarget.getName());
-				}
+//				}
 				return;
 			} else {
 				ErrorUtils.tptoggle(player, args.getArgs()[0]);
